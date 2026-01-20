54cd07741cfcabbf6604ff0ac0cf8a0a2ad99d99
Adityasudan21 <adityasudan21@gmail.com>
2026-01-21 00:55:58 +0530

    Did 1) Create bean in Apllication context through class type, without typcasting and without name. 2) Creating Inner Bean to limit the scope of bean to the outer bean only.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -17,13 +17,18 @@ public class App
         System.out.println("In Office");
 
         Programmer Programmer1= (Programmer) ap.getBean("p1"); //Object is created here if the scope of bean is prototype
+        Programmer Programmer2= ap.getBean("p1",Programmer.class); // This is also a way to intialize bean with Casting
+        Programmer Programmer3= ap.getBean(Programmer.class); //This is way to generally getbean byType in the config (primary attribute
+        //help if two or more beans of Programmer.class are there to prevent conflict and error
+
+
 //        Computer obj2=(Computer) ap.getBean("c1");
 //        Programmer1.code();
         Programmer1.setAge(23);
 
         System.out.println("Age of Programmer 1: "+Programmer1.getAge());
 
-        Programmer Programmer2= (Programmer) ap.getBean("p1");
+//        Programmer Programmer2= (Programmer) ap.getBean("p1");
 
         System.out.println("Age of Programmer 2: "+Programmer2.getAge());
 
